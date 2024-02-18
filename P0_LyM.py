7151ff2a2689010d53ed7459ff8710abec75ac64
Tdiazv2 <t.diazv@uniandes.edu.co>
2024-02-18 16:08:07 -0500

    a

--- P0_LyM.py
+++ P0_LyM.py
@@ -1,8 +1,25 @@
-comandos = {"defvar": None, "move":None, "skip":None, "turn":None, "face":None, "put":None, "pick":None}
 
-codigo = open("archivo", "r")
-linea = codigo.readline()
-while(linea!=""):
+def lectura():
+
+    comandos = {"defvar": None, "move":None, "skip":None, "turn":None, "face":None, "put":None, "pick":None}
+
+    Dim = 0
+    myXpos = 0
+    myYpos = 0
+    myChips = 0
+    myBalloons = 0
+    ballonsHere = 0
+    ChipsHere = 0
+    Spaces = 0
+
+    codigo = open("archivo", "r")
     linea = codigo.readline()
+    funciona = True
+    while(linea!="" and funciona == True):
+        linea = codigo.readline()
     
-codigo.close()
+    codigo.close()
+
+def defvar(comando):
+    comando.lower().replace("(", "").replace(")","")
+    
